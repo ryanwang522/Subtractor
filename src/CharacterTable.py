b81b81d22fb67c8b3fbc11369d8e306ada204867
RyanWang <perfect3366@gmail.com>
2018-05-11 01:37:12 +0800

    Update the source code for directly execute

--- src/CharacterTable.py
+++ src/CharacterTable.py
@@ -1,22 +1,34 @@
 import numpy as np
 
 class CharacterTable(object):
-    def __init__(self, chars="0123456789- "):
+    def __init__(self, chars="0123456789+-"):
         self.chars = sorted(set(chars))
-
+        self.ansChars = sorted(set('0123456789'))
+        
         # build correspondence table between char and index of encoding
         self.charToIndex = dict((c, i) for i, c in enumerate(self.chars))
         self.indexToChar = dict((i ,c) for i, c in enumerate(self.chars))
+        self.ansCharsToIndex =  dict((c, i) for i, c in enumerate(self.ansChars))
+        self.indexToAnsChars = dict((i ,c) for i, c in enumerate(self.ansChars))
 
-    def encode(self, expr, rowsNum):
-        x = np.zeros(shape=(rowsNum, len(self.chars)))
-        for i, c in enumerate(expr):
-            x[i, self.charToIndex[c]] = 1
+    def encode(self, expr, rowsNum, type):
+        if type == "expr":
+            x = np.zeros(shape=(rowsNum, len(self.chars)))
+            for i, c in enumerate(expr):
+                x[i, self.charToIndex[c]] = 1
+        elif type == "ans":
+            x = np.zeros(shape=(rowsNum, len(self.ansChars)))
+            for i, c in enumerate(expr):
+                x[i, self.ansCharsToIndex[c]] = 1
+
         return x 
 
-    def decode(self, x, argmax=True):
+    def decode(self, x, type, argmax=True):
         if argmax:
             # get the index of max value(=1) of each row
             x = x.argmax(axis=1)
-        # then look up indexToChar table to decode
-        return "".join(self.indexToChar[i] for i in x)
+        # then look up table to decode
+        if type == "expr":
+            return "".join(self.indexToChar[i] for i in x)
+        elif type == "ans":
+            return "".join(self.indexToAnsChars[i] for i in x)
--- src/main.py
+++ src/main.py
@@ -1,35 +1,41 @@
 import numpy as np
-from CharacterTable import *
-from keras.models import Sequential
+import random
+from keras.models import Sequential, load_model
 from keras import layers
+from CharacterTable import *
+import argparse
 
 class colors:
     ok = '\033[92m'
     fail = '\033[91m'
     close = '\033[0m'
 
-def genData(quesSize=80000, digits=3):
+def genData(quesSize=150000, digits=3):
     questions = []
     expected = []
     seen = set()
 
     print('Generating data...')
 
-    while len(questions) < quesSize:
-        func = lambda: int(''.join(np.random.choice(list('0123456789'))
+    func = lambda: int(''.join(np.random.choice(list('0123456789'))
                         for i in range(np.random.randint(1, digits + 1))))
+    while len(questions) < quesSize:
         a, b = func(), func()
         key = tuple(sorted((a, b)))
         if key in seen:
             continue
         seen.add(key)
+        
+        if random.randint(0, 99) % 2 == 0:
+            e = str(a).zfill(digits) + '-' + str(b).zfill(digits)
+            ans = str(a - b)
+        else:
+            e = str(a).zfill(digits) + '+' + str(b).zfill(digits)
+            ans = str(a + b).zfill(4)
+        
+        ans = "1" + ans[1:].zfill(4) if int(ans) < 0 else "0" + ans.zfill(4)
 
-        e = '{}-{}'.format(a, b)
-        expression = e + " " * ((2 * digits + 1) - len(e))
-        ans = str(a - b)
-        ans += " " * (digits + 1 - len(ans)) # `+1` for the carry or the minus sign.
-
-        questions.append(expression)
+        questions.append(e)
         expected.append(ans)
     
     print('Generating data complete.')
@@ -39,23 +45,19 @@ def genData(quesSize=80000, digits=3):
 def Vectorize(questions, expected, ctable, digits=3):
     print('Vectorization begin...')
     MAXLEN = 2 * digits + 1
-    x = np.zeros((len(questions), MAXLEN, len(ctable.chars)), dtype=np.bool)
-    y = np.zeros((len(expected), digits + 1, len(ctable.chars)), dtype=np.bool)
+    x = np.zeros((len(questions), MAXLEN, len(ctable.chars)))
+    y = np.zeros((len(expected), digits + 2, len(ctable.ansChars)))
 
     # encoding the each char in expression/answer to boolean value
     for i, sentence in enumerate(questions):
-        x[i] = ctable.encode(sentence, MAXLEN)
+        x[i] = ctable.encode(sentence, MAXLEN, "expr")
     for i, sentence in enumerate(expected):
-        y[i] = ctable.encode(sentence, digits + 1)
+        y[i] = ctable.encode(sentence, digits + 2, "ans")
     
     print('Vectorization complete')
     return x, y
 
-def splitData(x, y, splitAt=20000):
-    indices = np.arange(len(y))
-    np.random.shuffle(indices) # why shuffle ??
-    x = x[indices]
-    y = y[indices]
+def splitData(x, y, splitAt=120000):
 
     # train_test_split
     train_x = x[:splitAt]
@@ -63,77 +65,122 @@ def splitData(x, y, splitAt=20000):
     test_x = x[splitAt:]
     test_y = y[splitAt:]
 
-    # train validation split
-    splitAt = len(train_x) - len(train_x) // 10
-    (val_x, train_x) = train_x[splitAt:], train_x[:splitAt]
-    (val_y, train_y) = train_y[splitAt:], train_y[:splitAt]
+    # train validation split for RNN
+    # splitAt = len(train_x) - len(train_x) // 10
+    # (val_x, train_x) = train_x[splitAt:], train_x[:splitAt]
+    # (val_y, train_y) = train_y[splitAt:], train_y[:splitAt]
 
-    return ((train_x, train_y), (val_x, val_y), (test_x, test_y))
+    # return ((train_x, train_y), (val_x, val_y), (test_x, test_y))
+    return ((train_x, train_y), (test_x, test_y))
 
-def main():
-    ctable = CharacterTable("0123456789- ")
-
-    questions, expected = genData()
-    x, y = Vectorize(questions, expected, ctable)
-    
-    ((train_x, train_y), (val_x, val_y), (test_x, test_y)) = splitData(x, y)
-
-    print('Build model...')
+def buildModel(train_x, train_y):
+    print('Building model...')
     HIDDEN_SIZE = 128
     BATCH_SIZE = 128
-    LAYERS = 1
-    EPOCHS = 1
     digits = 3
+    
+    # flattern the data
+    train_x = train_x.reshape(len(train_x), -1) # 7 * 12 -> 1 * 84
+    print(train_x.shape)
 
-    model = Sequential()
-    model.add(layers.LSTM(HIDDEN_SIZE, input_shape=(2 * digits + 1, len(ctable.chars))))
-    model.add(layers.RepeatVector(digits + 1))
-    for _ in range(LAYERS):
-        model.add(layers.LSTM(HIDDEN_SIZE, return_sequences=True))
+    train_y_sign = train_y[:, 0, :2].reshape(len(train_y), -1)
+    train_y_num = train_y[:, 1:len(train_y), :].reshape(len(train_y), -1)
+    print(train_y_num.shape)
+    print(train_y_sign.shape)
+    
+    modelOfNums = Sequential()
+    modelOfNums.add(layers.Dense(250 , input_shape=(84,), activation='relu'))
+    modelOfNums.add(layers.Dense(250, activation='relu'))
+    modelOfNums.add(layers.Dense(150, activation='relu'))
+    modelOfNums.add(layers.Dense(100, activation='relu'))
+    modelOfNums.add(layers.Dense(4*10, activation='sigmoid'))
+    modelOfNums.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    modelOfNums.fit(train_x, train_y_num,
+                    batch_size=BATCH_SIZE,
+                    validation_split=0.2, 
+                    shuffle=True, verbose=1, epochs=100)
+    
+    modelOfSign = Sequential()
+    modelOfSign.add(layers.Dense(250 , input_shape=(84,), activation='relu'))
+    modelOfSign.add(layers.Dense(250, activation='relu'))
+    modelOfSign.add(layers.Dense(150, activation='relu'))
+    modelOfSign.add(layers.Dense(50, activation='relu'))
+    modelOfSign.add(layers.Dense(2, activation='sigmoid'))
+    modelOfSign.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy']) 
+    modelOfSign.fit(train_x, train_y_sign,
+                    batch_size=BATCH_SIZE,
+                    validation_split=0.2, 
+                    shuffle=True, verbose=1, epochs=80)
+    
+    return modelOfNums, modelOfSign
 
-    model.add(layers.TimeDistributed(layers.Dense(len(ctable.chars))))
-    model.add(layers.Activation('sigmoid'))
-    model.compile(loss='categorical_crossentropy',
-                optimizer='adam',
-                metrics=['accuracy'])
-    model.summary()
+def transform(ans):
+    if ans[0] == '1':
+        ans = "-" + ans[1:len(ans)]
+    return int(ans)
 
-    for iteration in range(100):
-        print()
-        print('-' * 50)
-        print('Iteration', iteration)
-        model.fit(train_x, train_y,
-                batch_size=BATCH_SIZE,
-                epochs=EPOCHS,
-                validation_data=(val_x, val_y))
-        for i in range(10):
-            ind = np.random.randint(0, len(val_x))
-            rowx, rowy = val_x[np.array([ind])], val_y[np.array([ind])]
-            preds = model.predict_classes(rowx, verbose=0)
-            q = ctable.decode(rowx[0])
-            correct = ctable.decode(rowy[0])
-            guess = ctable.decode(preds[0], argmax=False)
-            print('Q', q, end=' ')
-            print('T', correct, end=' ')
+def validation(modelOfNums, modelOfSign, test_x, test_y, ctable):
+    print('Validation with testing data...')
+    right = 0
+
+    predNums = modelOfNums.predict(test_x.reshape(len(test_x), -1), verbose=0)
+    predNums = predNums.reshape((len(test_y), 4, 10))
+    predSign = np.argmax(modelOfSign.predict(test_x.reshape(len(test_x), -1), verbose=0), axis=1)
+
+    for i in range(len(predSign)):
+        q = ctable.decode(test_x[i], type="expr")
+        correct = transform(ctable.decode(test_y[i], type="ans"))
+        
+        guessNum = ctable.decode(predNums[i], type="ans")
+        if predSign[i] == 0:
+            guessSign = "+"
+        elif predSign[i] == 1:
+            guessSign = "-"
+        guess = int(guessSign + guessNum)
+        
+        if correct == guess:
+                right += 1
+        
+        if i >= len(predSign) - 10:
+            print(q, end=' ')
+            print(correct, end='\t')
             if correct == guess:
                 print(colors.ok + '☑' + colors.close, end=' ')
             else:
                 print(colors.fail + '☒' + colors.close, end=' ')
-            print(guess)    
-    
-    print('Validation with testing data...')
-    right = 0
-    preds = model.predict_classes(test_x, verbose=0)
-    
-    for i in range(len(preds)):
-        q = ctable.decode(test_x[i])
-        correct = ctable.decode(test_y[i])
-        guess = ctable.decode(preds[i], argmax=False)
-        if correct == guess:
-            right += 1
-        
-    print("MSG : Accuracy is {}".format(right / len(preds)))
+            print(guess)
 
+    print('Validation complete.')        
+    print("Accuracy : {0:3.2f} %".format((right / len(predNums)) * 100), end=' ')
+    print("({} / {})".format(right, len(predNums)))
+
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--retrain',
+                        default=False,
+                        help='input T/F to retrain model')
+    args = parser.parse_args()
+
+    # Generate data
+    ctable = CharacterTable()
+    questions, expected = genData()
+    x, y = Vectorize(questions, expected, ctable)
+    ((train_x, train_y), (test_x, test_y)) = splitData(x, y)
+ 
+    if not args.retrain:
+        # Use the best model so far
+        modelDir = "../model/"
+        modelOfNums = load_model(modelDir + "nums_model")
+        modelOfSign = load_model(modelDir + "sign_model")
+        
+        validation(modelOfNums, modelOfSign, test_x, test_y, ctable)
+    else:
+        # retrain the model by training data and output the accuracy result
+        modelOfNums, modelOfSign = buildModel(train_x, train_y)
+        validation(modelOfNums, modelOfSign, test_x, test_y, ctable)
+        
 
 if __name__ == "__main__":
     main()
